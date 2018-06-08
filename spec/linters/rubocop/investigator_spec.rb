c23ffffee366123b9aa044f5abebfb6c200baa78
Volmer Campos Soares <git@radicaos.com>
2018-06-08 14:35:36 -0400

    Improve investigator specs

--- spec/linters/rubocop/investigator_spec.rb
+++ spec/linters/rubocop/investigator_spec.rb
@@ -3,7 +3,13 @@
 require 'spec_helper'
 
 describe Policial::Linters::RuboCop::Investigator do
-  let(:file) { double('file', filename: 'app/test.rb', content: 'puts "foo"') }
+  let(:file) do
+    double(
+      'file',
+      filename: 'app/test.rb',
+      content: "# Test code\n\ndef hello()\n  puts 'Hey!'\nend"
+    )
+  end
   let(:config) { ::RuboCop::ConfigLoader.default_configuration }
   subject { described_class.new(file, config) }
 
@@ -14,26 +20,26 @@ describe Policial::Linters::RuboCop::Investigator do
       expect(violations.count).to eq 3
 
       expect(violations[0].filename).to eq('app/test.rb')
-      expect(violations[0].line_range).to eq(1..1)
+      expect(violations[0].line_range).to eq(5..5)
       expect(violations[0].linter).to eq('Layout/TrailingBlankLines')
       expect(violations[0].message).to eq(
         'Layout/TrailingBlankLines: Final newline missing.'
       )
 
       expect(violations[1].filename).to eq('app/test.rb')
-      expect(violations[1].line_range).to eq(1..1)
-      expect(violations[1].linter).to eq('Style/FrozenStringLiteralComment')
+      expect(violations[1].line_range).to eq(3..3)
+      expect(violations[1].linter).to eq('Style/DefWithParentheses')
       expect(violations[1].message).to eq(
-        'Style/FrozenStringLiteralComment: Missing magic comment '\
-        '`# frozen_string_literal: true`.'
+        'Style/DefWithParentheses: Omit the parentheses in defs when the '\
+        "method doesn't accept any arguments."
       )
 
       expect(violations[2].filename).to eq('app/test.rb')
       expect(violations[2].line_range).to eq(1..1)
-      expect(violations[2].linter).to eq('Style/StringLiterals')
+      expect(violations[2].linter).to eq('Style/FrozenStringLiteralComment')
       expect(violations[2].message).to eq(
-        "Style/StringLiterals: Prefer single-quoted strings when you don't "\
-        'need string interpolation or special symbols.'
+        'Style/FrozenStringLiteralComment: Missing magic comment '\
+        '`# frozen_string_literal: true`.'
       )
     end
   end
